a8c73348d6960fb7383bcf7f84aef155185446db
siddhant-agrawal01 <siddhantagrawal001@gmail.com>
2026-03-15 10:36:41 +0530

    add authservice

--- services/authService.js
+++ services/authService.js
@@ -4,9 +4,25 @@ import jwt from "jsonwebtoken"
 
 const register = async (userData) => {
 
+    const user = new User(userData);
+    await user.save();
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, {
+        expiresIn: "1h"
+    })
+    return { user, token }
 }
 
 const login = async ({ email, password }) => {
+    const user = await User.findOne({ email })
+    if (!user || !(await user.comparePassword(password))) {
+        throw new Error("Invalid email or password")
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+    return { user, token }
 
 }
 
