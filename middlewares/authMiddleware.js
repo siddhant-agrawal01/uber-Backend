48738e4fab926315ab1650f123ef55eb7250bd53
siddhant-agrawal01 <siddhantagrawal001@gmail.com>
2026-03-15 10:48:19 +0530

    added the auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,8 +3,17 @@ import jwt from 'jsonwebtoken'
 import User from '../models/user.js'
 
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer', '')
+    if (!token) return res.status(401).send('Access Denied');
 
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = await User.findById(verified.id)
+        next()
+    } catch (error) {
+        res.status(400).send('Invalid Token')
+    }
 }
 
 export default authMiddleware
